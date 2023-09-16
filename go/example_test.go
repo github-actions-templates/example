22ee14631d5e1afee17ca62e24c239abe72efc45
Flc゛ <four_leaf_clover@foxmail.com>
2023-09-16 21:35:51 +0800

    init

--- go/example_test.go
+++ go/example_test.go
@@ -3,13 +3,7 @@ package example
 import "testing"
 
 func TestExample(t *testing.T) {
-	// success example
 	if Example() != "Hello, world!" {
-		t.Error("Expected 'Hello, world!'")
-	}
-
-	// error example
-	if Example() != "Hello, world" {
 		t.Error("Expected 'Hello, world!'")
 	}
 }

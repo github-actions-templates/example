5a72b67b8ab1f98ada8377f6f6d658695cdd4e60
Flc゛ <four_leaf_clover@foxmail.com>
2023-09-18 23:11:46 +0800

    go

--- go/example.go
+++ go/example.go
@@ -1,6 +1,6 @@
 package example
 
-// Context from Code Snippet .github/workflows/go-test.yml:name: Go Test
+// Example Context from Code Snippet .github/workflows/go-test.yml:name: Go Test
 func Example() string {
 	return "Hello, world!"
 }
